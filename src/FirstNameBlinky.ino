b1ec06cca6864b3e9cddd59634dafee2a72e3777
Ashley Whamond <ashleywhamond@gmail.com>
2022-04-15 22:27:51 +1000

    initial morse setup

--- src/FirstNameBlinky.ino
+++ src/FirstNameBlinky.ino
@@ -6,16 +6,31 @@
  */
 int light = D7;
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  pinMode(light, OUTPUT);
 
+const int UNIT = 300;               // single Morse unit
+const int DOT = UNIT;               // dot duration
+const int DASH = UNIT * 3;          // dash duration
+const int CHAR_SPACE = UNIT;        // space between parts of letter
+const int LETTER_SPACE = UNIT * 3;  // space between letters
+const int END_WORD = UNIT * 7;    // space between words
+
+const int letterLength = 300; // write function to determein letter translations
+
+
+void letter(){
+  int count = 0;
+  while (count < letterLength){
+    digitalWrite(light, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(DOT);                        // display for length of DOT
+    digitalWrite(light, LOW);    // turn the LED off by making the voltage LOW
+    delay(CHAR_SPACE);                 // wait for length of 
+    count++;
+  }
+  delay(LETTER_SPACE);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
+
+// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(light, HIGH);
-  delay(250);
-  digitalWrite(light, LOW);
-  delay(250);
+  delay(END_WORD);
 }
